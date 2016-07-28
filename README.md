40c014cabfb22cd1fc2701e3d408a30647ae5885
lwxyfer <lwxyfer@gmail.com>
2016-07-28 11:38:03 +0800

    fix rules

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 quickly color your background and text
 
-# USAGE
+# 使用
 
-include lazyColor.css into your head
+1. 直接在html中引入:
+<link src="'/path/to/dist/lazyColor.min.css'">
+
+也可以使用webpack直接打包。
+
+2.
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var pages = require('gulp-gh-pages');
+var rename = require('gulp-rename');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
 var minifyCss = require('gulp-clean-css');
@@ -12,6 +14,11 @@ gulp.task('less',function() {
 gulp.task('build',function() {
   gulp.src('src/lazyColor.less')
   .pipe(less())
+  .pipe(gulp.dest('dist'))
+  .pipe(minifyCss())
+  .pipe(rename({
+    suffix:'.min'
+  }))
   .pipe(gulp.dest('dist'))
 })
 
